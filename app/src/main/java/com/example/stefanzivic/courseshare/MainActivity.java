447b6a7700900de57bbbb6c7b4148406aeedf5bd
Stefan Zivic <stefanzivic9@gmail.com>
2017-07-03 18:20:57 +0200

    Navigation drawer items click listeners added

--- app/src/main/java/com/example/stefanzivic/courseshare/MainActivity.java
+++ app/src/main/java/com/example/stefanzivic/courseshare/MainActivity.java
@@ -80,20 +80,19 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        if(id == R.id.all_lectures_item) {
 
         }
+        else if (id==R.id.all_teachers_item) {
+
+        }
+        else if(id == R.id.favourite_teachers_item) {
+
+        }
+        else if(id == R.id.pending_lectures_item) {
+
+        }
+
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
