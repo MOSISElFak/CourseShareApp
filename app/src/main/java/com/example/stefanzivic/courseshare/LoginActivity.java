2a28e9eacbc9e78d5631c8889e4b721288c111e4
Stefan Zivic <stefanzivic9@gmail.com>
2017-07-03 16:05:21 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stefanzivic/courseshare/LoginActivity.java
@@ -0,0 +1,111 @@
+package com.example.stefanzivic.courseshare;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser user;
+    private Button signupButton,loginButton;
+    private EditText emailText,passwordText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailText = (EditText)findViewById(R.id.text_email);
+        passwordText = (EditText)findViewById(R.id.text_password);
+        signupButton = (Button)findViewById(R.id.signup_button);
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignupActivity();
+            }
+        });
+        loginButton = (Button)findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser(emailText.getText().toString(),passwordText.getText().toString());
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (user != null) {
+                    // User is signed in
+                    Log.d("TAG", "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d("TAG", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null ) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    public void loginUser(String email,String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("TAG", "signInWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w("TAG", "signInWithEmail:failed", task.getException());
+                            Toast.makeText(LoginActivity.this, "Auth failed",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            user = FirebaseAuth.getInstance().getCurrentUser();
+                            Toast.makeText(LoginActivity.this,user.getEmail().toString(),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    public void openSignupActivity() {
+        Intent sigup = new Intent(LoginActivity.this,LoginActivity.class);
+    }
+
+
+}
